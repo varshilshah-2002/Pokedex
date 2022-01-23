a46f4a6970370ae11225585471b84b5c1cf87666
Varshil <>
2022-01-23 23:46:10 +0530

    changes made

--- src/Start.jsx
+++ src/Start.jsx
@@ -3,13 +3,11 @@ import {BrowserRouter} from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/index.jsx'
 import Pokemon from './App.js';
-import Footer from './components/Footer.js'
 function Start() {
   return <div>
       <BrowserRouter>
       <Routes>
-            <Route exact path="/" element={<Home/>} />
-            <Route exact path="/F" element={<Footer/>} />
+            <Route exact path="/Home" element={<Home/>} />
             <Route exact path="/pokedex" element={<Pokemon/>}/>
         </Routes>
       </BrowserRouter>
